8751e2109746de3b1f3b07ae454f9141caf5204f
balexvic <balexvicx@gmail.com>
2019-10-18 10:22:26 +0200

    ready

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports  = {
-  BACKEND_URL : "http://localhost:4000/",
-//  BACKEND_URL : "http://converterserver-env.ebzaewej33.us-east-2.elasticbeanstalk.com/", 
-  RATE_PLN_API: "http://api.nbp.pl/api/exchangerates/tables/A",
+  //BACKEND_URL : "http://localhost:4000/",
+  BACKEND_URL : "http://converterserver-env.ebzaewej33.us-east-2.elasticbeanstalk.com/", 
+  //BACKEND_URL : "https://eurpln-server.herokuapp.com/",
+  RATE_PLN_API: "https://api.nbp.pl/api/exchangerates/tables/A",
   MAX_RATE_DIFF_IN_PROCENT: 10
 }
